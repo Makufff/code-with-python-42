de15a03b0957ca19be3c29b2cc5abedd99a8a1d2
makufff <makufff.tanapat@gmail.com>
2026-09-22 02:54:07 +0700

    add : cell 05

---
+++ discovery_piscine/cell05/ex13/append_it.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) == 1:
+    print("none")
+else:
+    for param in sys.argv[1:]:
+        if not param.endswith("ism"):
+            print(param + "ism")
