149a2265ffe335c7d3efca16cd7d4bd49d1df684
makufff <makufff.tanapat@gmail.com>
2026-09-22 02:53:57 +0700

    add : cell 02

---
+++ discovery_piscine/cell02/ex00/iszero.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+print("This number is different from zero.") if int(input()) != 0 else print("This number is equal to zero.")
---
+++ discovery_piscine/cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+password = "Python is awesome"
+
+entered = input()
+
+if entered == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
